ebabf45f333c2c35a0947e9d66ea4be9dbda840f
Robbert Tuerlings <robberttg@protonmail.com>
2021-01-31 11:28:50 +0100

    Code cleanup

--- src/components/OverviewHeader.tsx
+++ src/components/OverviewHeader.tsx
@@ -2,15 +2,9 @@ import React from 'react'
 import SearchBar from './SearchBar'
 import HeaderOptions from './HeaderOptions'
 import './OverviewHeader.scss'
-import { useHistory } from 'react-router-dom'
 import TitleHeader from './TitleHeader'
 
 export default function Overviewheader() {
-    const history = useHistory()
-
-    function navigateToHome() {
-        history.push('/')
-    }
 
     return (
         <div>
