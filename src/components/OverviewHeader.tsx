09d70f13677cf78bf153ab7e34d646c78ecabf57
Robbert Tuerlings <robberttg@protonmail.com>
2021-02-07 23:51:22 +0100

    Fix bugs CI

--- src/components/OverviewHeader.tsx
+++ src/components/OverviewHeader.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import SearchBar from './SearchBar'
 import HeaderOptions from './HeaderOptions'
 import './OverviewHeader.scss'
 import TitleHeader from './TitleHeader'
-// import { FiSearch } from 'react-icons/fi'
-// import { useHistory } from 'react-router-dom'
 import Searchbutton from './SearchButton'
 
 export default function Overviewheader() {
