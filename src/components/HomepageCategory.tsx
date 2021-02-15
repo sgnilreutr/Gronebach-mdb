c93738017bac7a1716216fe35a81edec3e2ef850
Robbert Tuerlings <robberttg@protonmail.com>
2021-02-15 22:56:45 +0100

    Update styling

--- src/components/HomepageCategory.tsx
+++ src/components/HomepageCategory.tsx
@@ -2,10 +2,22 @@ import React from 'react'
 import { FiChevronRight } from 'react-icons/fi'
 import { Movie } from '../data/api'
 import MovieListContainer from './MovieListContainer'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import './Homepage.scss'
 
 const CATEGORY_CTA_TEXT = 'Bekijk alles'
 
 const HomepageCategory = ({ movieList, categoryName, categoryFilter }: { movieList: Movie[], categoryName: string, categoryFilter: string }) => {
+    const dispatch = useDispatch()
+    const history = useHistory();
+
+    const openCategory = (value: string) => {
+        dispatch({ type: 'SET_OVERVIEW_QUERY', payload: value })
+        history.push({
+            pathname: `/overview/${ value }`
+        })
+    }
 
     function sliceMovies() {
         if (movieList) {
@@ -25,15 +37,15 @@ const HomepageCategory = ({ movieList, categoryName, categoryFilter }: { movieLi
 
     return (
         <div>
-            <div className="category-header">
+            <div onClick={() => openCategory(categoryFilter)} className="category-header">
                 <h2 className="category-header__title">
                     {categoryName}
                 </h2>
-                <div className="category-header__subtitle__nohover">
+                <FiChevronRight className="category-header__icon" size={18} />
+                <div className="category-header__subtitle">
                     <span className="category-header__subtitle-text">
                         {CATEGORY_CTA_TEXT}
                     </span>
-                    <FiChevronRight className="category-header__icon" size={18} />
                 </div>
             </div>
             <MovieListContainer movies={randomSlice} />
