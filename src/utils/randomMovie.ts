281b69b46ac339bbe85472e373cb79eb439d68a7
Robbert <robberttg@protonmail.com>
2021-09-18 17:48:33 +0200

    Update randomMovie.ts

--- src/utils/randomMovie.ts
+++ src/utils/randomMovie.ts
@@ -9,13 +9,13 @@ export const randomMovie = (movies: MovieSearch[]) => {
 //  Make it only loop for 10 times
 //  Make it take out the movie from the pool of possibilties once it is taken
 export const tenRandomMovies = (movies: MovieSearch[]) => {
-  if (movies.length > 10) {
+  const movieProp = movies
+  if (movieProp.length > 10) {
     const movieListBuffer = []
     for (let count = 0; count < 10; count += 1) {
       const randomNumber = Math.floor(Math.random() * (movies.length - 1)) + 1
-      movieListBuffer.push(movies[randomNumber])
-      // eslint-disable-next-line no-param-reassign
-      movies.splice(randomNumber, 1)
+      movieListBuffer.push(movieProp[randomNumber])
+      movieProp.splice(randomNumber, 1)
     }
     return movieListBuffer
   }
