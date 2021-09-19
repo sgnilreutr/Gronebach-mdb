878706255d4bcff5e1041a498008436956729d34
Robbert <robberttg@protonmail.com>
2021-09-19 14:50:49 +0200

    Fix width slider + fixed content loading

--- src/components/MovieOverview/MovieList.tsx
+++ src/components/MovieOverview/MovieList.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './MovieList.scss'
-import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import ScrollMenu from 'react-horizontal-scrolling-menu'
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
@@ -12,33 +11,20 @@ interface Props {
   movies: Movie[]
 }
 
-const selectCategory = (state: any) => state.category
-
 const MovieList: React.FC<Props> = ({ movies }) => {
-  const category = useSelector(selectCategory)
   const [menuItems, setMenuItems] = useState<any>([])
-  const [filteredMovies, setFilteredMovies] = useState<Movie[]>([])
   const isMountedRef = useRef<boolean | null>(null)
 
-  useEffect(() => {
-    if (movies.length > 0) {
-      const moviesFiltered = movies.filter((movie) =>
-        movie.Type.toLowerCase().includes(category.toLowerCase())
-      )
-      setFilteredMovies(moviesFiltered)
-    }
-  }, [category, movies])
-
   const Menu = () =>
-    filteredMovies.map((movie: any) => (
+    movies.map((movie: any) => (
       <MovieListItem
         key={movie.imdbID}
         movieInfo={{
-          Title: `${movie.Title}`,
-          imdbID: `${movie.imdbID}`,
-          Poster: `${movie.Poster}`,
-          Genre: `${movie.Genre}`,
-          Type: `${movie.Type}`,
+          Title: `${ movie.Title }`,
+          imdbID: `${ movie.imdbID }`,
+          Poster: `${ movie.Poster }`,
+          Genre: `${ movie.Genre }`,
+          Type: `${ movie.Type }`,
         }}
       />
     ))
@@ -57,21 +43,21 @@ const MovieList: React.FC<Props> = ({ movies }) => {
   // Create menu from items
   useEffect(() => {
     isMountedRef.current = true
-    if (filteredMovies.length > 0) setMenuItems(Menu())
+    if (movies.length > 0) setMenuItems(Menu())
     return () => {
       isMountedRef.current = false
     }
-  }, [filteredMovies])
+  }, [movies])
 
   const renderMovies = () => (
     <div>
-      {filteredMovies.length > 0 && window.innerWidth >= global.WINDOW_WIDTH_416 && (
+      {movies.length > 0 && window.innerWidth >= global.WINDOW_WIDTH_416 && (
         <ScrollMenu data={menuItems} arrowLeft={ArrowLeft} arrowRight={ArrowRight} wheel={false} />
       )}
-      {filteredMovies.length > 0 && window.innerWidth < global.WINDOW_WIDTH_416 && (
+      {movies.length > 0 && window.innerWidth < global.WINDOW_WIDTH_416 && (
         <div className="movie-horizontal-grid">
           <div className="hs hs-scroll">
-            {filteredMovies.map((movie) => {
+            {movies.map((movie) => {
               const { Title, imdbID, Poster, Genre, Type } = movie
               return (
                 <MovieListItem
@@ -89,7 +75,7 @@ const MovieList: React.FC<Props> = ({ movies }) => {
           </div>
         </div>
       )}
-      {filteredMovies.length < 1 && (
+      {movies.length < 1 && (
         <div>
           <p>{global.NO_ITEMS}</p>
           <Link to="/missing">{global.LINK_MISSING_TITLE}</Link>
