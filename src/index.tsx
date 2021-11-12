934b3ba46d1472285aaeb7f8d460538f1ee05cd8
Robbert <robberttg@protonmail.com>
2021-11-12 12:30:15 +0100

    Initial service worker implementation

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import App from './App'
 import RootReducer from './reducers/reducer'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const store = createStore(RootReducer)
 
@@ -16,3 +17,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
