5bb0d0adc199d49bdb2354495e918e1cde3c48ef
Robbert <robberttg@protonmail.com>
2023-03-26 20:07:28 +0200

    hotfix unavailable filter

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -9,64 +9,71 @@ import SearchBar from '../Elements/SearchBar'
 import MovieListItem from '../MovieOverview/MovieListItem'
 import { useAppSelector } from '../../store/hooks'
 import { selectSearchTerm } from '../../store/appSlice'
+import type { IMovie } from '../../data/dataTypes'
 
 const START_SEARCHING = 'Start met zoeken'
+
+const RenderSearchMovies = ({
+  movies,
+  searchTerm,
+}: {
+  movies: Array<IMovie>
+  searchTerm: string
+}) => {
+  const filteredMovies = movies.filter(
+    ({
+      Title,
+      Year,
+      Type,
+      Runtime,
+      Genre,
+      Director,
+      Actors,
+      Country,
+      Plot,
+      imdbRating,
+    }) =>
+      (
+        Title.toLowerCase() +
+        Year +
+        Type.toLowerCase() +
+        Runtime.toLowerCase() +
+        Genre.toLowerCase() +
+        Director.toLowerCase() +
+        Actors.toLowerCase() +
+        Country.toLowerCase() +
+        Plot.toLowerCase() +
+        imdbRating
+      ).includes(searchTerm.toLowerCase())
+  )
+
+  return (
+    <div className="movie-grid">
+      {filteredMovies.length > 0 ? (
+        filteredMovies.map(({ Title, imdbID, Poster }) => (
+          <div key={imdbID}>
+            <MovieListItem
+              movieInfo={{
+                Title,
+                imdbID,
+                Poster,
+              }}
+            />
+          </div>
+        ))
+      ) : (
+        <div className="search-no-results">
+          <p>{global.NO_ITEMS}</p>
+          <Link to="/missing">{global.LINK_MISSING_TITLE}</Link>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default function Search() {
   const searchTerm = useAppSelector(selectSearchTerm)
   const { movies } = useContext(MovieDatabaseContext)
-
-  const renderSearchMovies = () => {
-    const filteredMovies = movies.filter(
-      ({
-        Title,
-        Year,
-        Type,
-        Runtime,
-        Genre,
-        Director,
-        Actors,
-        Country,
-        Plot,
-        imdbRating,
-      }) =>
-        (
-          Title.toLowerCase() +
-          Year +
-          Type.toLowerCase() +
-          Runtime.toLowerCase() +
-          Genre.toLowerCase() +
-          Director.toLowerCase() +
-          Actors.toLowerCase() +
-          Country.toLowerCase() +
-          Plot.toLowerCase() +
-          imdbRating
-        ).includes(searchTerm.toLowerCase())
-    )
-
-    return (
-      <div className="movie-grid">
-        {filteredMovies.length > 0 ? (
-          filteredMovies.map(({ Title, imdbID, Poster }) => (
-            <div key={imdbID}>
-              <MovieListItem
-                movieInfo={{
-                  Title,
-                  imdbID,
-                  Poster,
-                }}
-              />
-            </div>
-          ))
-        ) : (
-          <div className="search-no-results">
-            <p>{global.NO_ITEMS}</p>
-            <Link to="/missing">{global.LINK_MISSING_TITLE}</Link>
-          </div>
-        )}
-      </div>
-    )
-  }
 
   return (
     <div>
@@ -75,7 +82,9 @@ export default function Search() {
         <CloseButton />
       </div>
       {movies.length === 0 ? <h2>{global.LOADING}</h2> : null}
-      {movies.length > 0 && searchTerm ? renderSearchMovies() : null}
+      {movies.length > 0 && searchTerm ? (
+        <RenderSearchMovies movies={movies} searchTerm={searchTerm} />
+      ) : null}
       {movies.length > 0 && !searchTerm ? (
         <h2 className="search-page-placeholder">{START_SEARCHING}</h2>
       ) : null}
--- src/utils/filteredMovieList.ts
+++ src/utils/filteredMovieList.ts
@@ -8,14 +8,20 @@ interface IFilteredList {
 export default function filteredList({ activeFilter, movies }: IFilteredList) {
   switch (activeFilter) {
     case 'kids':
-      return movies.filter(({ Rated }) => Rated === 'PG' && 'G')
+      return Array.isArray(movies)
+        ? movies.filter(({ Rated }) => Rated === 'PG' && 'G')
+        : []
     case 'top':
-      return movies.filter(({ imdbRating }) => parseInt(imdbRating, 10) >= 8.0)
+      return Array.isArray(movies)
+        ? movies.filter(({ imdbRating }) => parseInt(imdbRating, 10) >= 8.0)
+        : []
     case 'all':
       return movies
     default:
-      return movies.filter(({ Genre }) =>
-        Genre.toLowerCase().includes(`${activeFilter}`)
-      )
+      return Array.isArray(movies)
+        ? movies.filter(({ Genre }) =>
+            Genre.toLowerCase().includes(`${activeFilter}`)
+          )
+        : []
   }
 }
