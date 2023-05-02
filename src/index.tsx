f33e187a31b5151b32c04c083b39d9132e2b99cd
Robbert <robberttg@protonmail.com>
2023-05-02 21:41:16 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.scss'
 import { Provider } from 'react-redux'
+import { inject } from '@vercel/analytics'
 import { store } from './store/store'
 
 import App from './App'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
-import { inject } from '@vercel/analytics';
 
-inject();
+inject()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
