acf0299ea473ed07bdf7dc8f1876c94431462964
sgnilreutr <sgnilreutr@users.noreply.github.com>
2025-10-30 08:34:18 +0100

    Simplifies label for darkmode toggle

--- src/components/Elements/ModeSwitch/SwitchDarkMode.tsx
+++ src/components/Elements/ModeSwitch/SwitchDarkMode.tsx
@@ -8,8 +8,8 @@ export function SwitchDarkMode() {
     <div className='flex items-center group'>
       <label
         className='pr-[15px] text-[15px] leading-none dark:text-gray-200 group-hover:opacity-100 md:opacity-0 transition-opacity ease-in-out'
-        htmlFor='airplane-mode'>
-        {isDark ? 'Activate Light mode' : 'Activate Dark mode'}
+        htmlFor='dark-mode'>
+        {isDark ? 'Licht' : 'Donker'}
       </label>
       <SwitchPrimitives.Root
         checked={isDark}
